1fcaa1726786da6a608f4c647a160b9bdfc3f324
BillyBernard <billybernard54@gmail.com>
2021-08-13 03:59:01 -0600

    taking a break from JS starting CSS

--- index.js
+++ index.js
@@ -248,9 +248,11 @@ Example born in 1901 and died in 1959 - included -- born in 1889 and died in 192
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 // Hint - Look up the .split() method
 
-function get20s(/*Your Code Here*/) {
+function get20s(array) {
   /*Your Code Here*/
+  return array.split(names[i]["1900 - 2000"]);
 }
+
 
 
 
@@ -263,9 +265,18 @@ Use removeArtist to do the following:
 
 For example, if removeArtist is invoked with the artists array and the number 0, it will remove Amedeo Modigliani from our dataset and return the number 19. */
 
-function removeArtist(/*Your Code Here*/) {
+function removeArtist(array, index) {
   /*Your Code Here*/
+  for(let i = 0; i < array.length; i++){
+    if(array[i] === index){
+      array.splice(i, 1);
+    }
+  }
+  return array[i];
 }
+
+
+
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -284,8 +295,10 @@ Use addArtist to do the following:
 
 Example: addArtist(artists) should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/) {
+function addArtist(array, id, name, years, genre, nationality, bio) {
   /*Your Code Here*/
+  array.push({id, name, years, genre, nationality, bio});
+  return array;
 }
 
 
@@ -297,8 +310,9 @@ Use lotsOfArt to do the following:
 
 For example lotsOfArt(artists); will return ["Amedeo Modigliani", "Rene Magritte", ... "Albrecht Dürer"]*/
 
-function lotsOfArt(/*Your Code Here*/) {
+function lotsOfArt(array) {
   /*Your Code Here*/
+  return array
 }
 
 
